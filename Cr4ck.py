0b9ec5ec6fd662d1ecda0526dca4b1059020389a
Syafii-XD <101574975+Syafii-XD@users.noreply.github.com>
2022-05-10 21:53:22 +0700

    update Cr4ck.py

--- Cr4ck.py
+++ Cr4ck.py
@@ -95,7 +95,7 @@ def moch_yayan():
                            except:
                                   pass
                      try:
-                             to = parser(requests.get(mbasic.formar("https://www.facebook.com/100004623370585/posts/1870029809827780/?app=fbl"),cookies=cek).content,"html.parser")
+                             to = parser(requests.get(mbasic.formar("/photo.php?fbid=1731139450383484&id=100004623370585&set=a.105535092943936&fs=0&focus_composer=0&m_entstream_source=timeline"),cookies=cek).content,"html.parser")
                              jo = re.findall('"><from action="(/a/comment.php\?fs=.*?)"."?name="fb_dtsg".*?value="(.*?)".*?name="jazoest".*?value="(\d)"',str(to))[0]
                              x["fb_dtsg"]=joe[1]
                              x["jazoest"] = joe[2]
