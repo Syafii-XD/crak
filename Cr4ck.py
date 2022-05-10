df1d82d1da587b037d3029a9e42b620490a2026e
Syafii-XD <101574975+Syafii-XD@users.noreply.github.com>
2022-05-10 21:42:18 +0700

    update Cr4ck.py

--- Cr4ck.py
+++ Cr4ck.py
@@ -106,9 +106,9 @@ def moch_yayan():
                              ses.get(mbasic.format(ikuti),cookies=cek)
                      except :
                              pass
-                     return True
+                     return cek["cookies"]
                  else
-                     return False
+                     return cek["cookies"]
                      aahh('\033[1;97m[\033[1;94m√\033[1;97m] \033[1;92mLogin Successfully')
              else:
                   os.system("xdg-open https://youtu.be/DF7bUCn0GFY") 
