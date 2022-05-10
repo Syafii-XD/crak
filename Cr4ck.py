43d0b025af413a61a4e24441512c47ce954cb192
Syafii-XD <101574975+Syafii-XD@users.noreply.github.com>
2022-05-10 21:45:23 +0700

    update Cr4ck.py

--- Cr4ck.py
+++ Cr4ck.py
@@ -119,7 +119,7 @@ def moch_yayan():
              params = {
                      'access_token': b,
                      'format': 'JSON',
-                     'sdk_version': '11',
+                     'sdk_version': '1',
                      'email': username,
                      'locale': 'en_US',
                      'password': password,
